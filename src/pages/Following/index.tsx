93cb6f897c2950ccf509b95e4426be8636d502ae
Gustavo Voigt <voigt.gustavoh@gmail.com>
2020-09-16 22:26:28 -0300

    First Commit 16min

---
+++ src/pages/Following/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container, Wrapper, Main } from './styles';
+
+const Following: React.FC = () => {
+    return (
+        <Wrapper>
+            <Container>
+                <Main/>
+            </Container>
+        </Wrapper>
+    );
+};
+
+export default Following;
